e6b50659a8156af8df4dc709ed790f5fb11a112e
Andre Furlanetti <andrebpf@usp.br>
2019-10-01 13:45:44 -0300

    Initial commit

---
+++ src/Lista_textos.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Lista_textos {
+    private static LinkedList<Texto> lista_textos;
+
+    public LinkedList<Texto> getLista_textos() {
+        return lista_textos;
+    }
+
+    public void setLista_textos(LinkedList<Texto> lista_textos) {
+        this.lista_textos = lista_textos;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Texto txt = new Texto();
+        Lista_textos lista = new Lista_textos();
+        char entrada;
+
+        while(true){
+            txt.texto = input.
+        }
+    }
+}
