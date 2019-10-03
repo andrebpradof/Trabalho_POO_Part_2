da78fa192480669f7e009ae4fd5227d0d6cbe8fa
Andre Furlanetti <andrebpf@usp.br>
2019-10-03 16:18:37 -0300

    teste inicial

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Texto txt = new Texto();
-        Lista_textos lista = new Lista_textos();
-        char entrada;
-
-        while(true){
-            txt.texto = input.
+        
         }
     }
 }
