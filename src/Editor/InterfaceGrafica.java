1d6ed0655d711b08cc34c4bbcc913599b3482493
modest0w <56058578+modest0w@users.noreply.github.com>
2019-10-27 22:33:13 -0300

    Ultimo commit

--- src/Editor/InterfaceGrafica.java
+++ src/Editor/InterfaceGrafica.java
@@ -6,9 +6,12 @@ import java.awt.*;
 public class InterfaceGrafica extends JFrame {
     private JMenu menuFile = new JMenu("Arquivo");
     private JMenu menuEdit = new JMenu("Editar");
+    private JMenu menuSair = new JMenu("Sair");
     private JMenuBar barra = new JMenuBar();
+    private JMenuItem menuItemColar = new JMenuItem("Colar");
     private JMenuItem menuItemAbrir = new JMenuItem("Abrir");
     private JMenuItem menuItemSalvar = new JMenuItem("Salvar");
+    private JMenuItem menuItemFechar = new JMenuItem("Fechar");
     private JMenuItem menuItemDesfazer = new JMenuItem("Desfazer");
     private JMenuItem menuItemRefazer = new JMenuItem("Refazer");
     private JMenuItem menuItemCopiar = new JMenuItem("Copiar");
@@ -16,8 +19,38 @@ public class InterfaceGrafica extends JFrame {
     private JMenuItem menuItemRemover = new JMenuItem("Remover");
     private JPanel painel = new JPanel();
     private TextArea textArea = new TextArea();
+    private JScrollPane scrollPane = new JScrollPane();
 
     public InterfaceGrafica(){
+        super("Editor de texto");
+
+        this.setLayout(new BorderLayout());
+
+        barra.add(menuFile);
+        barra.add(menuEdit);
+        barra.add(menuSair);
+
+        menuFile.add(menuItemAbrir);
+        menuFile.add(menuItemSalvar);
+        menuFile.add(menuItemFechar);
+
+        menuEdit.add(menuItemCopiar);
+        menuEdit.add(menuItemColar);
+        menuEdit.add(menuItemRecortar);
+        menuEdit.add(menuItemRemover);
+        menuEdit.add(menuItemDesfazer);
+        menuEdit.add(menuItemRefazer);
+
+        textArea.setEditable(true);
+        scrollPane.add(textArea);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        this.add(barra, BorderLayout.NORTH);
+        this.add(scrollPane, BorderLayout.CENTER);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setSize(800, 700);
 
     }
 }
--- src/Editor/Main.java
+++ src/Editor/Main.java
@@ -123,6 +123,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        menu();
+        InterfaceGrafica interfaceGrafica = new InterfaceGrafica();
     }
 }
