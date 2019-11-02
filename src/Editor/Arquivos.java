a17fecc64bc84f555150c97c37cd484f859dc14c
modest0w <56058578+modest0w@users.noreply.github.com>
2019-11-01 23:08:14 -0300

    modesto

--- src/Editor/Arquivos.java
+++ src/Editor/Arquivos.java
@@ -6,18 +6,17 @@ import java.util.LinkedList;
 import javax.swing.*;
 
 /**
- * Classe que responsavel pelas manipulacoes de arquivos para leitura e armazenamento
+ * Classe que responsavel pelas manipulacoes de arquivos para leitura de arquivos de texto e armazenamento
  * dos textos editados pelo usuario, bem como abertura de novos arquivos
  */
 
 public class Arquivos implements Serializable {
-    private static String filename = "texto.bin";
-    private static JFileChooser fileChooser;
-    private static File file;
-    private static FileWriter fileWriter;
-    private static BufferedWriter bufferedWriter;
-    private static FileReader fileReader;
-    private static BufferedReader bufferedReader;
+    private static JFileChooser fileChooser; //janela de seleção de arquivo ou pasta na maquina do usuario
+    private static File file; //arquivo a ser manipulado
+    private static FileWriter fileWriter; //ferramenta para salvar arquivo
+    private static BufferedWriter bufferedWriter; //buffer em que o texto editado é temporariamente armazenado enquanto é salvo no arquivo.
+    private static FileReader fileReader; //ferramenta de abertura de arquivo
+    private static BufferedReader bufferedReader; //buffer em que o texto do arquivo é temporariamente armazenado enquanto é aberto no editor.
 
     /**
      * Exibe na tela uma mensagem que pergunta se o usuario tem a intencao de salvar o arquivo novo arquivo
@@ -26,101 +25,103 @@ public class Arquivos implements Serializable {
      * salvar o arquivo, tendo a opcao de cancelar a operacao.
      * Ja para resposta negativa, a mensagem apenas deixara a tela o novo arquivo esta pronto para
      * ser editado, porem sem estar salvo na maquina do usuario.
-     * Independentemente da resposta, a area de texto da interface grafica e limpa, permitindo que o usuario
-     * inicie um novo texto.
+     * Independentemente da resposta, a area de texto da interface grafica e limpa.
      * @param textArea Area de texto da interface grafica.
      */
     public static void saveQuestion(JTextArea textArea){
-        if(textArea.getText().equals("")){
+        if(textArea.getText().equals("")){ //se o texto está em branco, retorna-se à edição.
             textArea.setText("");
         }
         else {
             int resposta = JOptionPane.showConfirmDialog(InterfaceGrafica.getFrames()[0], "Gostaria de salvar o arquivo?", "Novo", JOptionPane.YES_NO_OPTION);
-            if(resposta == 0)
-                salvar(textArea);
-            textArea.setText("");
+            if(resposta == 0) //caso o usuario confirme a intenção de salvar o texto:
+                salvar(textArea); //chama a função salvar
+            textArea.setText(""); //limpa a area de texto
         }
     }
 
     /**
-     *
-     * @param textArea
+     * Funcao que cria um novo projeto de texto.
+     * Este metodo chama a funcao saveQuestion, passando como parametro a textArea que recebeu.
+     * Sendo assim a area de texto sera limpa, permitindo o inicio do novo projeto.
+     * @param textArea Area de texto da interface grafica, que sera onde se iniciara o novo texto.
      */
     public static void novo(JTextArea textArea){
         saveQuestion(textArea);
     }
 
+    /**
+     * Funcao que permite ao usuario salvar o texto editado em um arquivo em sua maquina.
+     * A funcao abre uma janela com a estrutura de pastas e arquivos da maquina do usuario para que ele possa
+     * escolher onde salvara o arquivo e digitar o nome que deseja dar ao mesmo.
+     * Uma vez que isso esteja definido, quando o usuario clica em salvar o arquivo e salvo
+     * de acordo com esses dados. Caso o usuario clique no botao "cancel" da janela a qualquer momento
+     * aparece uma mensagem indicando que a ooperacao foi cancelada e nada e salvo, retornando a edicao do texto.
+     * @param textArea Area de texto da interface contendo o texto que se deseja salvar.
+     */
     public static void salvar(JTextArea textArea){
-        // Create an object of JFileChooser class
-        fileChooser = new JFileChooser("f:");
+        fileChooser = new JFileChooser("f:"); //o usuario seleciona o arquivo a ser lido na janela JFileChooser
 
-        // Invoke the showsSaveDialog function to show the save dialog
         int resposta = fileChooser.showSaveDialog(null);
 
-        if (resposta == JFileChooser.APPROVE_OPTION) {
+        if (resposta == JFileChooser.APPROVE_OPTION) { //caso o usuario clique em "Save"
 
-            // Set the label to the path of the selected directory
-            file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            file = new File(fileChooser.getSelectedFile().getAbsolutePath());//usuario seleciona onde deseja salvar o arqivo e que nome dá-lo
 
-            try {
-                // Create a file writer
+            try { //o texto é salvo no novo arquivo
                 fileWriter = new FileWriter(file, true);
 
-                // Create buffered writer to write
                 bufferedWriter = new BufferedWriter(fileWriter);
 
-                // Write
                 bufferedWriter.write(textArea.getText());
 
                 bufferedWriter.flush();
                 bufferedWriter.close();
             }
-            catch (Exception evt) {
+            catch (Exception evt) {//Exibe uma mensagem de erro caso algo dê errado no salvamento
                 JOptionPane.showMessageDialog(InterfaceGrafica.getFrames()[0], evt.getMessage());
             }
         }
-        // If the user cancelled the operation
+        //Caso o usuario cancele a operação uma mensagem é exibida
         else
             JOptionPane.showMessageDialog(InterfaceGrafica.getFrames()[0], "Operação cancelada");
     }
 
+    /**
+     * Funcao que possibilita a abertura no editor de um texto presente em um arquivo preexistente na maquina do usuario.
+     * Abre-se a uma janela contendo as pastas e arquivos da maquina do usuario, sendo que quando selecionado um arqivo,
+     * ao clicar-se no botao "Open", o texto do arquivo e transferido para a area de texto.
+     * @param textArea Area de texto da interface, para onde deseja-se transferir o texto do arquivo aberto.
+     */
     public static void abrir(JTextArea textArea){
-            fileChooser = new JFileChooser("f:");
+            fileChooser = new JFileChooser("f:"); //o usuario seleciona o arquivo que deseja abrir na janela JFileChooser.
 
-            // Invoke the showsOpenDialog function to show the save dialog 
             int resposta = fileChooser.showOpenDialog(null);
 
-            // If the user selects a file 
-            if (resposta == JFileChooser.APPROVE_OPTION) {
-                // Set the label to the path of the selected directory 
+            if (resposta == JFileChooser.APPROVE_OPTION) { //caso o usuario clique em "Open"
+
                 file = new File(fileChooser.getSelectedFile().getAbsolutePath());
 
                 try {
-                    // String 
-                    String s1 = "", sl = "";
-
-                    // File reader 
+                    String s1 = "", sl = ""; //strings para auxiliar a passagem do texto do arquivo para a area de texto da interface
                     fileReader = new FileReader(file);
 
-                    // Buffered reader 
                     bufferedReader = new BufferedReader(fileReader);
 
-                    // Initilize sl
-                    sl = bufferedReader.readLine();
+                    sl = bufferedReader.readLine(); //a string sl recebe o conteudo da primeira linha do buffer
 
-                    // Take the input from the file
-                    while ((s1 = bufferedReader.readLine()) != null) {
-                        sl = sl + "\n" + s1;
+                    while ((s1 = bufferedReader.readLine()) != null) { //s1 le uma linha do buffer por vez
+                        sl = sl + "\n" + s1; //a linha lida por s1 é acrescentada à string sl com uma quebra de linha
                     }
+                    //sl agora tem o texto completo do arquivo
 
-                    // Set the text 
-                    textArea.setText(sl);
+                    textArea.setText(sl); //o texto de sl é adicionado à area de texto da interface.
                 }
-                catch (Exception evt) {
+                catch (Exception evt) { //caso haja algum nerro na abertura do arquivo, uma mensagem de erro é exibida
                     JOptionPane.showMessageDialog(InterfaceGrafica.getFrames()[0], evt.getMessage());
                 }
             }
-            // If the user cancelled the operation 
+            //caso o usuario cancele a operação, uma mensagem é exibida e retorna-se ao editor.
             else
                 JOptionPane.showMessageDialog(InterfaceGrafica.getFrames()[0], "Operação cancelada");
     }
--- src/Editor/InterfaceGrafica.java
+++ src/Editor/InterfaceGrafica.java
@@ -20,24 +20,23 @@ import javax.swing.undo.UndoManager;
 
 public class InterfaceGrafica extends JFrame{
 
-    private JMenu menuFile = new JMenu("Arquivo");
-    private JMenu menuEdit = new JMenu("Editar");
-    private JMenu menuOpcao = new JMenu("Opções");
-    private JMenuItem menuItemSair = new JMenuItem("Sair");
-    private JMenuBar barra = new JMenuBar();
-    private JMenuItem menuItemNovo = new JMenuItem("Novo");
-    private JMenuItem menuItemColar = new JMenuItem("Colar");
-    private JMenuItem menuItemAbrir = new JMenuItem("Abrir");
-    private JMenuItem menuItemSalvar = new JMenuItem("Salvar");
-    private JMenuItem menuItemDesfazer = new JMenuItem("Desfazer");
-    private JMenuItem menuItemRefazer = new JMenuItem("Refazer");
-    private JMenuItem menuItemCopiar = new JMenuItem("Copiar");
-    private JMenuItem menuItemRecortar = new JMenuItem("Recortar");
- //   private JMenuItem menuItemRemover = new JMenuItem("Remover");
-    private JMenuItem menuItemSelecionar = new JMenuItem("Selecionar");
-    private JScrollPane scrollPane;
-    private JTextArea textArea = new JTextArea();
-    private UndoManager undoManager = new UndoManager();
+    private JMenu menuFile = new JMenu("Arquivo"); //menu de manipulação de arquivos
+    private JMenu menuEdit = new JMenu("Editar"); //menu de edição de texto
+    private JMenu menuOpcao = new JMenu("Opções"); //menu de opções
+    private JMenuItem menuItemSair = new JMenuItem("Sair"); //botão para sair do editor
+    private JMenuBar barra = new JMenuBar(); //barra de menus
+    private JMenuItem menuItemNovo = new JMenuItem("Novo"); //botao para iniciar uma nova edição
+    private JMenuItem menuItemColar = new JMenuItem("Colar"); //botao para colar texto copiado ou recortado
+    private JMenuItem menuItemAbrir = new JMenuItem("Abrir"); //botao para abrir no editor o texto de um arquivo preexistente
+    private JMenuItem menuItemSalvar = new JMenuItem("Salvar"); //botao para salvar em um arquivo o texto editado
+    private JMenuItem menuItemDesfazer = new JMenuItem("Desfazer"); //botao para desfazer a ultima ação de edição nao desfeita
+    private JMenuItem menuItemRefazer = new JMenuItem("Refazer"); //botao para refazer a ultima ação de edição desfeita e nao refeita
+    private JMenuItem menuItemCopiar = new JMenuItem("Copiar"); //botao para copiar um trecho texto selecionado
+    private JMenuItem menuItemRecortar = new JMenuItem("Recortar"); //botao que copia o trecho selecionado e o apaga do texto
+    private JMenuItem menuItemSelecionar = new JMenuItem("Selecionar"); //botao para selecionar o texto inteiro
+    private JScrollPane scrollPane; //painel com barra de rolagem
+    private JTextArea textArea = new JTextArea(); //area de texto da interface
+    private UndoManager undoManager = new UndoManager(); //ferramenta necessaria para a realizção das rotinas de "Desfazer" e "Refazer
 
     /** Construtor da classe InterfaceGrafica.
      * Inicializa a interface, estrutura a janela com a area de texto e a barra de ferramentas, com cada um dos
@@ -48,16 +47,18 @@ public class InterfaceGrafica extends JFrame{
      */
 
     public InterfaceGrafica(){
-        super("Editor de texto");
+        super("Editor de texto"); //nome que aparece na janela do editor
 
         this.setLayout(new BorderLayout());
 
+        //adiciona-se os menuItens aos seus respectivos menus
         menuFile.add(menuItemNovo);
         menuFile.add(menuItemAbrir);
         menuFile.add(menuItemSalvar);
 
         menuOpcao.add(menuItemSair);
 
+        //desabilita os botoes desfazer e refazer assim que se abre o editor
         menuItemDesfazer.setEnabled(false);
         menuItemRefazer.setEnabled(false);
 
@@ -69,47 +70,49 @@ public class InterfaceGrafica extends JFrame{
         menuEdit.add(menuItemDesfazer);
         menuEdit.add(menuItemRefazer);
 
+        //adiciona-se os menus às suas respectivas barras
         barra.add(menuFile);
         barra.add(menuEdit);
         barra.add(menuOpcao);
 
-        textArea.setEditable(true);
-        textArea.setBorder(new EmptyBorder(5,5,5,5));
+        textArea.setEditable(true);//habilita-se a interação do usuario com a area de texto
+        textArea.setBorder(new EmptyBorder(5,5,5,5)); //acrescenta-se uma margem à area de texto
 
+        //Organiza-se a estrutura da interface
         this.setJMenuBar(barra);
         scrollPane = new JScrollPane(textArea);
         this.getContentPane().add(scrollPane,BorderLayout.CENTER);
 
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800, 700);
-        this.setLocationRelativeTo(null);
+        this.setVisible(true); //habilita-se a visibilidade da interface
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //habilita-se o botao "X" a fechar a interface e encerrar o programa
+        this.setSize(800, 700); //declaração do tamanho da janela do editor
+        this.setLocationRelativeTo(null); //centraliza a janela do editor na tela
 
         menuItemNovo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Arquivos.novo(textArea);
-            }
+            } //chama-se o metodo Arquivos.novo quando o usuario clica em "Novo"
         });
 
         menuItemAbrir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Arquivos.abrir(textArea);
-            }
+            } //chama-se o metodo Arquivos.abrir quando o usuario clica em "Abrir"
         });
 
         menuItemSalvar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Arquivos.salvar(textArea);
-            }
+            } //chama-se o metodo Arquivos.salvar quando o usuario clica em "Salvar"
         });
 
         menuItemSair.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                System.exit(0); //fecha a interface e encerra o programa
             }
         });
 
@@ -117,40 +120,34 @@ public class InterfaceGrafica extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 textArea.copy();
-            }
+            } //copia o trecho de texto selecionado
         });
 
         menuItemColar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 textArea.paste();
-            }
+            } //cola o ultimo trecho que foi copiado
         });
 
         menuItemRecortar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 textArea.cut();
-            }
+            } //copia o trecho selecionado e o apaga do texto
         });
 /*
-        menuItemRemover.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textArea.setText(textArea.getText().replace(textArea.getSelectedText(),""));
-            }
-        });
 */
         menuItemDesfazer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
+                try{ //tenta-se desfazer
                     undoManager.undo();
                 }
                 catch (CannotUndoException cre){
                     cre.printStackTrace();
                 }
-                updateMenuItens();
+                updateMenuItens(); //chama updateMenuItens
             }
         });
 
@@ -158,35 +155,35 @@ public class InterfaceGrafica extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 try{
-                    undoManager.redo();
+                    undoManager.redo(); //refaz a ultima ação de edição desfeita não refeita
                 }
                 catch (CannotRedoException cre){
                     cre.printStackTrace();
                 }
-                updateMenuItens();
+                updateMenuItens(); //chama updateMenuItens
             }
         });
 
         menuItemSelecionar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textArea.selectAll();
+                textArea.selectAll(); //seleciona o texto inteiro da area de texto
             }
         });
 
         textArea.getDocument().addUndoableEditListener(new UndoableEditListener() {
             @Override
             public void undoableEditHappened(UndoableEditEvent e) {
-                undoManager.addEdit(e.getEdit());
-                updateMenuItens();
+                undoManager.addEdit(e.getEdit()); //adiciona o que foi editado na area de texto à ferramenta undoManager
+                updateMenuItens(); //chama updateMenuItens
             }
         });
     }
 
-    private void updateMenuItens(){
+    private void updateMenuItens(){ //habilita os botões "Desfazer" e "Refazer" conforme há ações para serem desfeitas ou refeitas
         //this.menuItemDesfazer.setText(undoManager.getUndoPresentationName());
         //this.menuItemRefazer.setText(undoManager.getRedoPresentationName());
-        this.menuItemDesfazer.setEnabled(undoManager.canUndo());
-        this.menuItemRefazer.setEnabled(undoManager.canRedo());
+        this.menuItemDesfazer.setEnabled(undoManager.canUndo()); //habilita "Desfazer"
+        this.menuItemRefazer.setEnabled(undoManager.canRedo()); //habilita o "Refazer"
     }
 }
